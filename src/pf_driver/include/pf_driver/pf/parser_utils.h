06c283d3e4afbf2b052057c2211367d1861ab982
EU\jhander <jhander@de.pepperl-fuchs.com>
2024-09-17 15:41:16 +0200

    both files added again from windows

---
+++ src/pf_driver/include/pf_driver/pf/parser_utils.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace parser_utils
+{
+const std::vector<std::string> split(const std::string& str, const char delim = ';');
+
+std::int64_t to_long(const std::string& s);
+
+float to_float(const std::string& s);
+
+}  // namespace parser_utils
