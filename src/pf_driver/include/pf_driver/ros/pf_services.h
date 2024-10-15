5e75bf18a3d851a7b5fdeafd8c6610721bcb4b5c
JnsHndr <jhander@de.pepperl-fuchs.com>
2024-10-15 17:47:00 +0200

    service without bind

--- src/pf_driver/include/pf_driver/ros/pf_services.h
+++ src/pf_driver/include/pf_driver/ros/pf_services.h
@@ -17,57 +17,57 @@
 #include "pf_interfaces/srv/pfsdp_reset_iq_parameter.hpp"
 #include "pf_interfaces/srv/pfsdp_list_iq_parameters.hpp"
 
-void pfsdp_reboot_device(PFInterface* pf,
-                         const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_reboot_device(//PFInterface* pf,
+                         //const std::shared_ptr<rmw_request_id_t> request_header,
                          const std::shared_ptr<pf_interfaces::srv::PfsdpRebootDevice::Request> request,
                          std::shared_ptr<pf_interfaces::srv::PfsdpRebootDevice::Response> response);
 
-void pfsdp_factory_reset(PFInterface* pf,
-                         const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_factory_reset(//PFInterface* pf,
+                         //const std::shared_ptr<rmw_request_id_t> request_header,
                          const std::shared_ptr<pf_interfaces::srv::PfsdpFactoryReset::Request> request,
                          std::shared_ptr<pf_interfaces::srv::PfsdpFactoryReset::Response> response);
 
-void pfsdp_get_protocol_info(PFInterface* pf,
-                             const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_get_protocol_info(//PFInterface* pf,
+                             //const std::shared_ptr<rmw_request_id_t> request_header,
                              const std::shared_ptr<pf_interfaces::srv::PfsdpGetProtocolInfo::Request> request,
                              std::shared_ptr<pf_interfaces::srv::PfsdpGetProtocolInfo::Response> response);
 
-void pfsdp_list_parameters(PFInterface* pf,
-                           const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_list_parameters(//PFInterface* pf,
+                           //const std::shared_ptr<rmw_request_id_t> request_header,
                            const std::shared_ptr<pf_interfaces::srv::PfsdpListParameters::Request> request,
                            std::shared_ptr<pf_interfaces::srv::PfsdpListParameters::Response> response);
 
-void pfsdp_get_parameter(PFInterface* pf,
-                         const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_get_parameter(//PFInterface* pf,
+                         //const std::shared_ptr<rmw_request_id_t> request_header,
                          const std::shared_ptr<pf_interfaces::srv::PfsdpGetParameter::Request> request,
                          std::shared_ptr<pf_interfaces::srv::PfsdpGetParameter::Response> response);
 
-void pfsdp_set_parameter(PFInterface* pf,
-                         const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_set_parameter(//PFInterface* pf,
+                         //const std::shared_ptr<rmw_request_id_t> request_header,
                          const std::shared_ptr<pf_interfaces::srv::PfsdpSetParameter::Request> request,
                          std::shared_ptr<pf_interfaces::srv::PfsdpSetParameter::Response> response);
 
-void pfsdp_reset_parameter(PFInterface* pf,
-                           const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_reset_parameter(//PFInterface* pf,
+                           //const std::shared_ptr<rmw_request_id_t> request_header,
                            const std::shared_ptr<pf_interfaces::srv::PfsdpResetParameter::Request> request,
                            std::shared_ptr<pf_interfaces::srv::PfsdpResetParameter::Response> response);
 
-void pfsdp_list_iq_parameters(PFInterface* pf,
-                              const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_list_iq_parameters(//PFInterface* pf,
+                              //const std::shared_ptr<rmw_request_id_t> request_header,
                               const std::shared_ptr<pf_interfaces::srv::PfsdpListIqParameters::Request> request,
                               std::shared_ptr<pf_interfaces::srv::PfsdpListIqParameters::Response> response);
 
-void pfsdp_get_iq_parameter(PFInterface* pf,
-                            const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_get_iq_parameter(//PFInterface* pf,
+                            //const std::shared_ptr<rmw_request_id_t> request_header,
                             const std::shared_ptr<pf_interfaces::srv::PfsdpGetIqParameter::Request> request,
                             std::shared_ptr<pf_interfaces::srv::PfsdpGetIqParameter::Response> response);
 
-void pfsdp_set_iq_parameter(PFInterface* pf,
-                            const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_set_iq_parameter(//PFInterface* pf,
+                            //const std::shared_ptr<rmw_request_id_t> request_header,
                             const std::shared_ptr<pf_interfaces::srv::PfsdpSetIqParameter::Request> request,
                             std::shared_ptr<pf_interfaces::srv::PfsdpSetIqParameter::Response> response);
 
-void pfsdp_reset_iq_parameter(PFInterface* pf,
-                              const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_reset_iq_parameter(//PFInterface* pf,
+                              //const std::shared_ptr<rmw_request_id_t> request_header,
                               const std::shared_ptr<pf_interfaces::srv::PfsdpResetIqParameter::Request> request,
                               std::shared_ptr<pf_interfaces::srv::PfsdpResetIqParameter::Response> response);
--- src/pf_driver/src/ros/pf_services.cpp
+++ src/pf_driver/src/ros/pf_services.cpp
@@ -5,92 +5,103 @@
 #include "pf_driver/ros/pf_services.h"
 #include "pf_driver/pf/pf_interface.h"
 
-void pfsdp_reboot_device(PFInterface* pf,
-                         const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_reboot_device(//PFInterface* pf,
+                         //const std::shared_ptr<rmw_request_id_t> request_header,
                          const std::shared_ptr<pf_interfaces::srv::PfsdpRebootDevice::Request> request,
                          std::shared_ptr<pf_interfaces::srv::PfsdpRebootDevice::Response> response)
 {
-  pf->pfsdp_reboot(response->error_code, response->error_text);
+  //pf->pfsdp_reboot(response->error_code, response->error_text);
+  printf("pfsdp_reboot_device() called");
 }
 
-void pfsdp_factory_reset(PFInterface* pf,
-                         const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_factory_reset(//PFInterface* pf,
+                         //const std::shared_ptr<rmw_request_id_t> request_header,
                          const std::shared_ptr<pf_interfaces::srv::PfsdpFactoryReset::Request> request,
                          std::shared_ptr<pf_interfaces::srv::PfsdpFactoryReset::Response> response)
 {
-  pf->pfsdp_factory(response->error_code, response->error_text);
+  //pf->pfsdp_factory(response->error_code, response->error_text);
+  printf("pfsdp_factory_reset() called");
 }
 
-void pfsdp_get_protocol_info(PFInterface* pf,
-                             const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_get_protocol_info(//PFInterface* pf,
+                             //const std::shared_ptr<rmw_request_id_t> request_header,
                              const std::shared_ptr<pf_interfaces::srv::PfsdpGetProtocolInfo::Request> request,
                              std::shared_ptr<pf_interfaces::srv::PfsdpGetProtocolInfo::Response> response)
 {
-  pf->pfsdp_info(response->protocol_name, response->version_major, response->version_minor, response->commands,
-                 response->error_code, response->error_text);
+  //pf->pfsdp_info(response->protocol_name, response->version_major, response->version_minor, response->commands,
+  //               response->error_code, response->error_text);
+  printf("pfsdp_get_protocol_info() called");
 }
 
-void pfsdp_list_parameters(PFInterface* pf,
-                           const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_list_parameters(//PFInterface* pf,
+                           //const std::shared_ptr<rmw_request_id_t> request_header,
                            const std::shared_ptr<pf_interfaces::srv::PfsdpListParameters::Request> request,
                            std::shared_ptr<pf_interfaces::srv::PfsdpListParameters::Response> response)
 {
-  pf->pfsdp_list("list_parameters", "parameters", response->parameters, response->error_code, response->error_text);
+  //pf->pfsdp_list("list_parameters", "parameters", response->parameters, response->error_code, response->error_text);
+  printf("pfsdp_list_parameters() called");
 }
 
-void pfsdp_get_parameter(PFInterface* pf,
-                         const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_get_parameter(//PFInterface* pf,
+                         //const std::shared_ptr<rmw_request_id_t> request_header,
                          const std::shared_ptr<pf_interfaces::srv::PfsdpGetParameter::Request> request,
                          std::shared_ptr<pf_interfaces::srv::PfsdpGetParameter::Response> response)
 {
-  pf->pfsdp_get("get_parameter", request->name, response->value, response->error_code, response->error_text);
+  //pf->pfsdp_get("get_parameter", request->name, response->value, response->error_code, response->error_text);
+  printf("pfsdp_get_parameter() called");
 }
 
-void pfsdp_set_parameter(PFInterface* pf,
-                         const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_set_parameter(//PFInterface* pf,
+                         //const std::shared_ptr<rmw_request_id_t> request_header,
                          const std::shared_ptr<pf_interfaces::srv::PfsdpSetParameter::Request> request,
                          std::shared_ptr<pf_interfaces::srv::PfsdpSetParameter::Response> response)
 {
-  pf->pfsdp_set("set_parameter", request->name, request->value, response->error_code, response->error_text);
+  //pf->pfsdp_set("set_parameter", request->name, request->value, response->error_code, response->error_text);
+  printf("pfsdp_set_parameter() called");
 }
 
-void pfsdp_reset_parameter(PFInterface* pf,
-                           const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_reset_parameter(//PFInterface* pf,
+                           //const std::shared_ptr<rmw_request_id_t> request_header,
                            const std::shared_ptr<pf_interfaces::srv::PfsdpResetParameter::Request> request,
                            std::shared_ptr<pf_interfaces::srv::PfsdpResetParameter::Response> response)
 {
-  pf->pfsdp_reset("reset_parameter", request->name, response->error_code, response->error_text);
+  //pf->pfsdp_reset("reset_parameter", request->name, response->error_code, response->error_text);
+  printf("pfsdp_reset_parameter() called");
 }
 
-void pfsdp_list_iq_parameters(PFInterface* pf,
-                              const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_list_iq_parameters(//PFInterface* pf,
+                              //const std::shared_ptr<rmw_request_id_t> request_header,
                               const std::shared_ptr<pf_interfaces::srv::PfsdpListIqParameters::Request> request,
                               std::shared_ptr<pf_interfaces::srv::PfsdpListIqParameters::Response> response)
 {
-  pf->pfsdp_list("list_iq_parameters", "iq_parameters", response->iq_parameters, response->error_code,
-                 response->error_text);
+  //pf->pfsdp_list("list_iq_parameters", "iq_parameters", response->iq_parameters, response->error_code,
+  //               response->error_text);
+  printf("pfsdp_list_iq_parameters() called");
 }
 
-void pfsdp_get_iq_parameter(PFInterface* pf,
-                            const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_get_iq_parameter(//PFInterface* pf,
+                            //const std::shared_ptr<rmw_request_id_t> request_header,
                             const std::shared_ptr<pf_interfaces::srv::PfsdpGetIqParameter::Request> request,
                             std::shared_ptr<pf_interfaces::srv::PfsdpGetIqParameter::Response> response)
 {
-  pf->pfsdp_get("get_iq_parameter", request->name, response->value, response->error_code, response->error_text);
+  //pf->pfsdp_get("get_iq_parameter", request->name, response->value, response->error_code, response->error_text);
+  printf("pfsdp_get_iq_parameter() called");
 }
 
-void pfsdp_set_iq_parameter(PFInterface* pf,
-                            const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_set_iq_parameter(//PFInterface* pf,
+                            //const std::shared_ptr<rmw_request_id_t> request_header,
                             const std::shared_ptr<pf_interfaces::srv::PfsdpSetIqParameter::Request> request,
                             std::shared_ptr<pf_interfaces::srv::PfsdpSetIqParameter::Response> response)
 {
-  pf->pfsdp_set("set_iq_parameter", request->name, request->value, response->error_code, response->error_text);
+  //pf->pfsdp_set("set_iq_parameter", request->name, request->value, response->error_code, response->error_text);
+  printf("pfsdp_set_iq_parameter() called");
 }
 
-void pfsdp_reset_iq_parameter(PFInterface* pf,
-                              const std::shared_ptr<rmw_request_id_t> request_header,
+void pfsdp_reset_iq_parameter(//PFInterface* pf,
+                              //const std::shared_ptr<rmw_request_id_t> request_header,
                               const std::shared_ptr<pf_interfaces::srv::PfsdpResetIqParameter::Request> request,
                               std::shared_ptr<pf_interfaces::srv::PfsdpResetIqParameter::Response> response)
 {
-  pf->pfsdp_reset("reset_iq_parameter", request->name, response->error_code, response->error_text);
+  //pf->pfsdp_reset("reset_iq_parameter", request->name, response->error_code, response->error_text);
+  printf("pfsdp_reset_iq_parameter() called");
 }
